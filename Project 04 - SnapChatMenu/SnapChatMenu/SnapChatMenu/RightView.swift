787877f3d1baa9efb6459870f11a3b71146a6313
gaki2745 <guyeongjun@naver.com>
2019-09-06 00:25:59 +0900

    [Project 4] Xib 사용법 과 CameraView, ScrollView 사용법 연습

---
+++ Project 04 - SnapChatMenu/SnapChatMenu/SnapChatMenu/RightView.swift
@@ -0,0 +1,22 @@
+//
+//  RightView.swift
+//  SnapChatMenu
+//
+//  Created by youngjun goo on 05/09/2019.
+//  Copyright © 2019 youngjun goo. All rights reserved.
+//
+
+import UIKit
+
+
+class RightView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
--- Project 04 - SnapChatMenu/SnapChatMenu/SnapChatMenu/ViewController.swift
+++ Project 04 - SnapChatMenu/SnapChatMenu/SnapChatMenu/ViewController.swift
@@ -15,20 +15,41 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setUpCameraView()
         
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
     func setUpCameraView() {
         let screenWidth = UIScreen.main.bounds.width
         let screenHeight = UIScreen.main.bounds.height
+        // https://zeddios.tistory.com/297 : xib 사용법
         guard let leftView: UIViewController = UINib(nibName: "LeftView", bundle: nil).instantiate(withOwner: nil, options: nil)[0] as? UIViewController else {
             return
         }
-        let centerView: CamerView =
+        let centerView: CameraView = CameraView(nibName: "CameraView", bundle: nil)
+        let rightView: RightView = RightView(nibName: "RightView", bundle: nil)
         
+        leftView.view.frame = CGRect(x: 0, y: 0, width: screenWidth-200, height: screenHeight)
+        centerView.view.frame = CGRect(x: screenWidth, y: 0, width: screenWidth, height: screenHeight)
+        rightView.view.frame = CGRect(x: 2*screenWidth, y: 0, width: screenWidth, height: screenHeight)
+        
+        // SCrollView 설정
+        self.scrollView.addSubview(leftView.view)
+        self.scrollView.addSubview(rightView.view)
+        self.scrollView.addSubview(centerView.view)
+        self.scrollView.contentSize = CGSize(width: screenWidth * 3, height: screenHeight)
         
     }
 
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    
 
 }
 
